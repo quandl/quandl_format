0d2f2ae4f89ac3142c3441d68f1ab31a6c70d4d7
Blake Hilscher <blake@hilscher.ca>
2013-12-07 20:53:41 -0500

    revise specs to have less decimals

--- examples/load.rb
+++ examples/load.rb
@@ -6,3 +6,4 @@ Quandl::Logger.use Quandl::Logger::Outputs
 Quandl::Format::Dataset.load_from_file('spec/fixtures/data/invalid_data.qdf')
 Quandl::Format::Dataset.load_from_file('spec/fixtures/data/invalid_yaml.qdf')
 Quandl::Format::Dataset.load_from_file('spec/fixtures/data/mismatched_columns.qdf')
+Quandl::Format::Dataset.load_from_file('spec/fixtures/data/mismatched_rows.qdf')
