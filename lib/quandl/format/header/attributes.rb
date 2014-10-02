c3df19061702e2ab4021bc99c3d72cf553c93585
Sergei Ryshkevich <sryshkev@gmail.com>
2014-10-02 15:05:02 -0400

    added replace option to header

--- lib/quandl/format/header/attributes.rb
+++ lib/quandl/format/header/attributes.rb
@@ -8,7 +8,7 @@ module Quandl
 
         module ClassMethods
 
-          ATTRIBUTES = :notify, :token
+          ATTRIBUTES = :notify, :token, :replace
 
           def attribute_names
             @attribute_names ||= ATTRIBUTES
